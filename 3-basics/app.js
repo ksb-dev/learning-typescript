04cbb6658aaad5d8fb6c2e90ffbf4ab7fe4472c2
Kedar Babaleshwar <babaleshwarkedar@gmail.com>
2023-03-07 20:36:21 +0530

    Initial Commit

---
+++ 3-basics/app.js
@@ -0,0 +1,17 @@
+/*
+
+Core types
+
+1. Number
+2. String
+3. Boolean
+
+
+*/
+function add(n1, n2) {
+    return n1 + n2;
+}
+var num1 = '5';
+var num2 = 10.2;
+var res = add(num1, num2);
+console.log(res);
---
+++ 3-basics/app.ts
@@ -0,0 +1,21 @@
+/* 
+
+Core types 
+
+1. Number 
+2. String 
+3. Boolean 
+
+
+*/
+
+function add (n1: number, n2: number) {
+  return n1 + n2
+}
+
+const num1 = '5'
+const num2 = 10.2
+
+const res = add(+num1, num2)
+
+console.log(res)
